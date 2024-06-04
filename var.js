d78a549f9743501f4f0ddb9bbff4c45ceb927565
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2024-06-04 15:04:41 +0600

    variable

---
+++ var.js
@@ -0,0 +1,29 @@
+//common four math operations (-_-) 
+
+
+//addition
+var gajor = 30;
+//var apple = 40;
+//var totalPrice = (gajor+apple);
+//console.log(totalPrice);
+// console.log(gajor);
+//  console.log(apple);
+// console.log(gajor+apple);
+
+
+//substraction
+// var mula = 90;
+// var sub = mula - gajor;
+// console.log(sub);
+
+
+//Multiplication
+// var kola = 7;
+// var mul = kola*gajor;
+// console.log(mul);
+
+
+//division
+var alu = 320;
+var div = alu/gajor;
+console.log(div);
