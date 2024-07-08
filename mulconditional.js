967ec106a2b633efe56ff291546d02f8d0e9e676
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2024-07-08 07:16:43 +0600

    did multiple conditionals

---
+++ mulconditional.js
@@ -0,0 +1,10 @@
+//Doing multiple conditional of JS
+var salary = 70000;
+var car = 2;
+var graduation = true;
+
+if (graduation === true && salary >= 50000 && car >= 1) {
+  console.log("Biye kroe felbo");
+} else {
+  console.log("Kopale biye nai");
+}
