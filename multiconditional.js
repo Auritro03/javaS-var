415a58311458b0d4bd8374283bef1c767b4405aa
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2024-07-08 07:35:38 +0600

    loop chalailam

---
+++ multiconditional.js
@@ -0,0 +1,17 @@
+var moneyHave = 20;
+var tea = 10;
+var puri = 20;
+var alurchop = 20;
+var shingara = 30;
+
+if(moneyHave>=puri){
+    console.log('cha diye puri khabo');
+}
+else if(moneyHave>=alurchop){
+    console.log('cha er shathe chop khabo');}
+    else if(moneyHave>shingara){
+        console.log('vorpet nasta korbo');
+    }
+    else {
+        console.log('cah kheye chole ashbo');
+    }
